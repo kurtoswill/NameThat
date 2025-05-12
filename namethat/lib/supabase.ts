bf444181f4e34e9b7d8d30e793e7cdd44b3708a6
Kurt Oswill <kurtoswillmc@gmail.com>
2025-05-12 16:14:50 +0800

    feat: supabase

--- namethat/lib/supabase.ts
+++ namethat/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://elfvxyrlaslnfyfxaxoi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZnZ4eXJsYXNsbmZ5ZnhheG9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzQ2MDUsImV4cCI6MjA2MjQxMDYwNX0.aPgi4Q69t4lOYKvfWY_u5pzcgfrWjPPxbyaUVyWzmcA'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
