af4213dd3cf57635faeddcdc20cb65938945943e
Kurt Oswill <kurtoswillmc@gmail.com>
2025-05-12 17:47:03 +0800

    style: connect wallet

--- namethat/components/walletAddress.tsx
+++ namethat/components/walletAddress.tsx
@@ -2,13 +2,37 @@
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-
 import React from "react";
 
 export function BaseWalletAddress() {
   return (
-    <div>
-      <ConnectButton chainStatus="icon" showBalance={false} />
-    </div>
+    <ConnectButton.Custom>
+      {({ account, chain, openConnectModal, openAccountModal, mounted }) => (
+        <button
+          type="button"
+          onClick={
+            !mounted || !account || !chain
+              ? openConnectModal
+              : openAccountModal
+          }
+          style={{
+            background: "#3A86FF",
+            color: "#FFFDF6",
+            borderRadius: "6px",
+            fontWeight: "600",
+            padding: "10.5px 12px",
+            border: "none",
+            cursor: "pointer",
+            transition: "background 0.2s",
+          }}
+        >
+          {!mounted
+            ? "Connect Wallet"
+            : !account || !chain
+            ? "Connect Wallet"
+            : `${account.displayName}`}
+        </button>
+      )}
+    </ConnectButton.Custom>
   );
 }
